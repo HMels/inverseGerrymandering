7889b6afde1cb3f0c641a51162abe192ea28be6c
HMels <haboldmels@gmail.com>
2023-01-30 15:15:12 +0100

    Finished 1st version
    It now is able to optimise the entropy and spread populations around. I will now focus on using actual parameters, and making sure groups are divided equally

--- dataset.py
+++ dataset.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 24 17:15:56 2023
+Created on Fri Jan 27 15:35:15 2023
 
 @author: Mels
 """
-
-import pandas as pd
 import numpy as np
-from scipy.optimize import minimize, shgo, differential_evolution
-from scipy.stats import entropy
 import tensorflow as tf
+import pandas as pd
 
 tf.config.run_functions_eagerly(True)  ### TODO should be False
 
-class dataset(tf.keras.Model):
+class Dataset(tf.keras.Model):
     '''
     The class dataset is able to take a dataset defined below and do calculations with it.
     
@@ -50,74 +47,62 @@ class dataset(tf.keras.Model):
         Right now entropy does not normalize data and thus each data has weird weights
         
     '''
-    def __init__(self, Data, Map=None):
-        super(dataset, self).__init__()
+    def __init__(self, Data):
+        super(Dataset, self).__init__()
         self.Data = tf.Variable(Data, trainable=False, dtype=tf.float32)
-        self.Map = ( tf.Variable(Map, trainable=True, dtype=tf.float32) if Map is not None 
-            else tf.Variable(tf.eye(self.Data.shape[0]), trainable=True, dtype=tf.float32) )
+        self.Map = tf.Variable(tf.eye(self.Data.shape[0]), trainable=True, dtype=tf.float32)
         
-        # The matrices used to calculate entropy, and their original values
+        # The matrix used to calculate entropy, and their original values
         self.Pk = tf.matmul(self.Data , tf.ones([Data.shape[1], Data.shape[0]]) )
         self.Pk.trainable = False
-        self.Map = tf.Variable(1, trainable=True, dtype=tf.float32)
-
-    
-    @tf.function
-    def call(self, Map):
-        #return tf.matmul(Map, self.Pk)
-        return Map * 3
         
     
     @tf.function
-    def map_data(self):
-        return tf.transpose(tf.matmul(self.Map, tf.transpose(self.Data) ) , (1,0,2) )
-
-
-    @tf.function(autograph=True)
-    def calculate_entropy(self, Pk):
-        """
-        Calculates the entropy of Data via sum( Pk * log( Pk / Qk ) )
+    def call(self, inputs):
+    # transforms the inputs according to the Map
+        return tf.matmul(self.normalise_map(), inputs)
+    
+    
+    @tf.function
+    def calculate_entropy(self, Plogits):
+    # takes in the (mapped) matrix Pk and creates Qk 
+    # then calculates the entropy via sum( Pk*log(Pk/Qk) )
+        Qlogits = tf.transpose(Plogits)
+        return tf.reduce_sum( Plogits * tf.math.log(Plogits / Qlogits) )
+    
+    
+    @tf.function
+    def calculate_loss(self, Plogits):
+    # Total loss with regularization
+        # Original loss function (entropy)
+        original_loss = self.calculate_entropy(Plogits)
         
-        Output:
-            entr: double 
-                A single value that represents the total entropy in the mapped neighbourhoods.
-        """
-        #Pk = tf.Variable( tf.matmul(Map, self.Pk) , trainable=False, dtype=tf.float32)
-        #Qk = tf.transpose(Pk)
-        #x = tf.Variable(tf.reduce_sum(Pk * tf.math.log(Pk / Qk)))
-        #x = tf.reduce_sum(tf.square(Pk-Qk))
-        #return tf.Variable( tf.reduce_sum( Pk * tf.math.log(Pk / Qk) , axis=2))
-        return tf.Variable( Pk )#tf.Variable( tf.reduce_sum( Pk ) )
+        # L1 regularization term to let the map be positive
+        L1_pos = tf.reduce_sum(tf.abs(tf.where(self.Map < 0., self.Map, 0.)))
+        
+        # L1 regularization term to let the map be normalised to one when summed up
+        #L1_norm = tf.reduce_sum(tf.where(tf.reduce_sum(self.Map, axis=1, keepdims=True) > 1., self.Map, 0))
+        
+        # total L1  reegularization
+        L1_reg = L1_pos #+ L1_norm
+        
+        return original_loss + L1_reg
     
     
-def train_step(model):
-    with tf.GradientTape() as tape:
-        tape.watch(model.trainable_variables)
-        logits = model(model.trainable_variables)
-        loss_value = model.calculate_entropy(logits)
-    grads = tape.gradient(loss_value, model.trainable_variables)
-    print('grads=', grads)
-    optimizer = tf.keras.optimizers.Adam()
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-    return loss_value
+    @tf.function
+    def normalise_map(self):
+        Map = self.Map / tf.reduce_sum(self.Map, axis=0)       # Divide each row by its sum
+        return Map
 
 
-
-@tf.function 
-def train(model, learning_rate=1, epochs=100, opt_fn=tf.optimizers.Adagrad, opt=None):
-    """
-    Training the optimizes on the data to find the right map.
-    """
-    if opt is None: opt=opt_fn(learning_rate)
-    for i in range(epochs):
-        # calculate and apply gradients
+    @tf.function
+    def train_step(self, input_data):
         with tf.GradientTape() as tape:
-            loss = model.calculate_entropy(model.trainable_weights)
-        grads = tape.gradient(loss, model.trainable_weights)
-        opt.apply_gradients(zip(grads, model.trainable_weights))
-
-        if i%100==0 and i!=0: print('iteration='+str(i)+'/'+str(epochs))
-    return loss
+            logits = self(input_data)
+            loss_value = self.calculate_loss(logits)
+        grads = tape.gradient(loss_value, self.trainable_variables)
+        optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        return loss_value
 
 
 #%% load data
@@ -134,21 +119,16 @@ Spreiding = np.array(SES[:,4].tolist()).astype(np.float32)              # numeri
 Data = np.concatenate([part_huishoudens[:,None,None],SES_WOA[:,None,None]],axis=2) # use matrix projecting? to get in right format 
 
 
-#%% load parameters
-age = np.array([40,45,30,55])[:,None]
-Data = dataset(age)
+#%%
+age = tf.Variable(np.array([40,45,30,55])[:,None], dtype=tf.float32)
+model = Dataset(age)
+input_data = model.Pk
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
-train_step(Data)
-
-
-"lets try to make our own gradient"
-
-Data1=dataset(age,Map=np.array([[1., 0., 0., 0.03],
-       [0., 1., 0., 0.],
-       [0., 0., 1., 0.],
-       [0., 0., 0., .97]]))
-#Data1.calculate_entropy()
-profit = tf.reduce_sum( Data.calculate_entropy()-Data1.calculate_entropy() )
-print("this should be bigger than 0: ", profit.numpy())
-
-"I could also make a super wrong example and then see if it can solve that, because there is a probability calculate entropy wrong and it's already optimal"
+# Training loop
+for i in range(500):
+    loss_value = model.train_step(input_data)
+    if i % 100 == 0:
+        print("Step: {}, Loss: {}".format(i, loss_value.numpy()))
+        
+print( tf.round( model.normalise_map() * 100 , 1 ) )
